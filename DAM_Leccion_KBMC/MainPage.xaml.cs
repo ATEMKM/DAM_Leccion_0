a769e548d8e9c4d6b5778eac7964f021d2b8dd27
ATEMKM <20030355@itesa.edu.mx>
2025-01-28 18:52:46 -0600

    Version_02: Instrucciones basicas en boton de caja de texto

--- DAM_Leccion_KBMC/MainPage.xaml.cs
+++ DAM_Leccion_KBMC/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DAM_Leccion_KBMC
+﻿using DAM_Leccion_KBMC.Model;
+
+namespace DAM_Leccion_KBMC
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,7 @@
         public MainPage()
         {
             InitializeComponent();
+            Ejecutar();
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +22,20 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        public void Ejecutar()
+        {
+            PersonaModel personaModel = new PersonaModel();
+
+            personaModel.Nombre = "Hola aqui estoy";
+
+            txtNombre.Text = personaModel.Nombre;
+        }
+
+        private void btnAceptar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Asistente del sistema","Se a guardado el reistro de la BD","Aceptar");
         }
     }
 
